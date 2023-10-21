4d7425e8359e26f4f0d22ced54a986ea9c31be39
Soliday Ek <87338107+solidayek@users.noreply.github.com>
2023-10-21 12:54:39 -0700

    Create CS22Project1.py
    add instructions for the project

---
+++ CS22Project1.py
@@ -0,0 +1,49 @@
+CS22Project1.py
+# CS 22 – Project 1
+# Follow these instructions exactly as they are written:
+# • Open a project in PyCharm named CS22Project1.
+# • Create a Python file in this project named CS22Project1.py.
+# • Copy and paste the header from the CS22CommentHeader.docx file in the
+# Files area of Canvas and complete the lines for your name, the assignment, and
+# the due date.
+# • Create a class named Athlete as follows:
+# o It has three data attributes: one for a name, one for a country, and one for
+# a sport
+# o It has a constructor (initializer) to initialize the three data attributes
+# o It has accessor (get) and mutator (set) methods for each attribute (six
+# methods total)
+# • Create a class named TeamSport as follows:
+# o It inherits the Athlete class
+# o It has two data attributes: one for the position the player plays and one for
+# the number of league championships the player won.
+# o It has a constructor (initializer) to initialize five data attributes (name,
+# country, sport, position, league championships)
+# o It has accessor (get) and mutator (set) methods for each of its two
+# attributes (four methods total)
+# • Create a class named IndividualSport as follows:
+# o It inherits the Athlete class
+# o It has two data attributes: one for the total winnings of the player and one
+# for the number of major championships the player won.
+# o It has a constructor (initializer) to initialize five data attributes (name,
+# country, sport, winnings, major championships)
+# o It has accessor (get) and mutator (set) methods for each of its two
+# attributes (four methods total)
+# • Create a main() program after all the class definitions and write code for it as
+# follows:
+# o Create a list named team and make its elements objects of the
+# TeamSport class, using the following data for each object:
+#  ‘WayneGretzky’,’Canada’,’Hockey’,’Center’,4
+#  ‘Tom Brady’,’United States’,’Football’,’Quarterback’,7
+#  ‘Lionel Messi’,’Argentina’,’Soccer’,’Forward’,14
+# o Create another list named indy and make its elements objects of the
+# IndividualSport class, using the following data for each object:
+#  ‘Roger Federer’,’Switzerland’,’Tennis’,130594339,20
+#  ‘Rory McIlroy’,’Northern Ireland’,’Golf’,125147336,4
+# o Using a for loop, display data for each object in the team list in two
+# sentences, followed by a blank line.
+# o Using a second for loop, display data for each object in the indy list in two
+# sentences, followed by a blank line.
+#  NOTE: You must use an f-string in all your outputs, especially to
+# format dollar amounts with commas and 2 decimal places. Look at
+# the CS31Ch2Lab2.pptx file from the Gaddis PowerPoints for
+# examples.
